be0e99625dceaf630d0536d8afe4ab275184a06e
kandreeva <kandreeva952@gmail.com>
2023-03-29 14:09:38 +0300

    ИСПРАВЛЕНО

--- Simonov1/Form1.cs
+++ Simonov1/Form1.cs
@@ -13,61 +13,53 @@ namespace Simonov1
 {
     public partial class Form1 : Form
     {
-        public string log_in = "";
-        public int role = 0;
+        public string log_in = "";//инициализируем логин пустой строкой
+        public int role = 0;//инициализируем роль нулевым значением 
 
-        SqlConnection con = new SqlConnection(@"Data Source = pc-2584d8\sysadmin; Initial Catalog = Deteiling; Integrated Security = true");
+        SqlConnection con = new SqlConnection("Server=db.edu.cchgeu.ru;DataBase=193_Starkov;User=193_Starkov;Password=Qq123123");//подключаемся к бд
 
-        int tryCounter = 0;
+        int tryCounter = 0;// счетчик на не верный ввод логина пароля
 
         Timer timer = new Timer();
         int sec = 30;
         public Form1()
         {
             InitializeComponent();
-            textBox2.PasswordChar = '*';
+            textBox2.PasswordChar = '*';//значения введенное в текст бокс 2 будет скрито знаком звездочка 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text != "" && textBox2.Text != "")
-                tryLogin();
+            if (textBox1.Text != "" && textBox2.Text != "")//если поля текст бокс 1 и 2 не пусто
+                tryLogin();// то вызываем метод проверки существования записи в бд
         }
-        private void timer_tick(object s, EventArgs e)
-        {
-            sec += -1;
-            button1.Text = $"Вход ({sec} сек.)";
-            if (sec == 0)
-            {
-                sec = 30;
-                timer.Stop();
-                button1.Enabled = true;
+        //private void timer_tick(object s, EventArgs e) // метод блокировки формы на 30 сек
+        //{
+        //    sec += -1;
+        //    button1.Text = $"Вход ({sec} сек.)";
+        //    if (sec == 0)
+        //    {
+        //        sec = 30;
+        //        timer.Stop();
+        //        button1.Enabled = true;
                 
-                button1.Text = "Вход";
-            }
-        }
+        //        button1.Text = "Вход";
+        //    }
+        //}
 
-        public void startTimer()
-        {
-            timer.Interval = 1000;
-            timer.Tick += new EventHandler(timer_tick);
-            timer.Start();
-            button1.Text = "Вход (30 сек.)";
-            button1.Enabled = false;
-          
-        }
+       
 
         public bool isAccHave(string login)
         {
             int id = -1;
-            con.Open();
-            SqlCommand cmd = new SqlCommand($"select id from users where login = '{login}'", con);
-            using (var reader = cmd.ExecuteReader())
+            con.Open();// открываем соединение с бд
+            SqlCommand cmd = new SqlCommand($"select id from users where login_user = '{login}'", con); // получаем айди из бд по логину
+            using (var reader = cmd.ExecuteReader()) // считываем полученное значение из бд
             {
                 if (reader.Read())
                     id = Convert.ToInt32(reader.GetInt64(0));
             }
-            con.Close();
+            con.Close();// закрываем соединение с бд обязательно
             if (id != -1)
                 return true;
             else
@@ -76,11 +68,12 @@ namespace Simonov1
 
         private void newTry()
         {
-            tryCounter++;
-            if (tryCounter == 3)
+            tryCounter++; // считаем сколько раз сработало условие не верного ввода учетной записи в методе tryLogin
+            if (tryCounter == 3) // если 3 раза условие сработало 
             {
-                tryCounter = 0;
-                Form2 ca = new Form2(this);
+                tryCounter = 0; // обнуляем значение счетчика 
+                startTimer();
+                Form2 ca = new Form2(this);// открываем форму капчи 
                 ca.ShowDialog();
             }
         }
@@ -89,31 +82,33 @@ namespace Simonov1
         {
             string login = textBox1.Text;
             string password = textBox2.Text;
-            if (isAccHave(login))
+            //записываем значения из текст боксов 1 и 2 в переменные 
+            if (isAccHave(login)) //вызываем метод который вернет нам логин 
+
             {
                 string passFromTable = "";
                 int role_id = 0;
-                con.Open();
-                SqlCommand cmd = new SqlCommand($"select pass, id_roles from dbo.users where login_user = '{login}'", con);
+                con.Open(); // опять открываем соедение с бд
+                SqlCommand cmd = new SqlCommand($"select pass, id_roles from dbo.users where login_user = '{login}'", con);// получем из бд по логину пароль и ид роли
                 using (var reader = cmd.ExecuteReader())
                 {
                     if (reader.Read())
                     {
-                        passFromTable = reader.GetString(0);
-                        role_id = Convert.ToInt32(reader.GetInt64(1));
+                        passFromTable = reader.GetString(0); // записали в переменную пароль
+                        role_id = Convert.ToInt32(reader.GetInt64(1));//записали в переменную ид права
                     }
                 }
-                con.Close();
+                con.Close();//закрыли соединнеие 
 
-                if (passFromTable == password)
+                if (passFromTable == password)// проверяем если получаем тот же пароль из бд что и ввели в текст бокс 
                 {
-                    if (role_id == 1)
+                    if (role_id == 1) // проверяем ид прав, если 1 то админ и открывает форму с одними правами
                     {
                         log_in = login;
                         role = role_id;
                         openWindow(0);
                     }
-                    else
+                    else // иначе, если, не админ то другие права
                     {
                         log_in = login;
                         role = role_id;
@@ -122,30 +117,56 @@ namespace Simonov1
                 }
                 else
                 {
-                    MessageBox.Show("Неправильно набран пароль", "Ошибка входа");
+                    MessageBox.Show("Неправильно набран пароль", "Ошибка входа");// если не верно введен пароль
                     newTry();
                 }
             }
             else
             {
-                MessageBox.Show("Такого аккаунта не существует", "Ошибка входа");
+                MessageBox.Show("Такого аккаунта не существует", "Ошибка входа");// если не верно и логин и пароль
                 newTry();
             }
+            
         }
 
         public void openWindow(int t)
         {
-            //if (t == 1)
-            //{
+            if (t == 1)
+            {
+                MessageBox.Show("1 prava");
             //    Sotr st = new Sotr(this);
             //    st.Show();
-            //}
-            //else
-            //{
-            //  //  Client cl = new Client(this);
-            //  //  cl.Show();
-            //}
-            //this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("2 prava");
+                //  //  Client cl = new Client(this);
+                //  //  cl.Show();
+            }
+            this.Hide();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sec += -1;
+            button1.Text = $"Вход ({sec} сек.)";
+            if (sec == 0)
+            {
+                sec = 30;
+                timer.Stop();
+                button1.Enabled = true;
+
+                button1.Text = "Вход";
+            }
+        }
+        public void startTimer()
+        {
+            timer.Interval = 1000;
+            timer.Tick += new EventHandler(timer1_Tick);
+            timer.Start();
+            button1.Text = "Вход (30 сек.)";
+            button1.Enabled = false;
+
         }
     }
 }
